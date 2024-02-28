26d818927c4cb80d2097156567952ed3d1aeef0e
nischay-code <nischay.patel.code@gmail.com>
2024-02-28 11:02:46 +0530

    Modification Done

--- firebase.js
+++ firebase.js
@@ -3,12 +3,6 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrgaCuuTv1gQ9ZVWPeuAkei8Qiwh6jYZU",
-  authDomain: "signal-clone-b30f8.firebaseapp.com",
-  projectId: "signal-clone-b30f8",
-  storageBucket: "signal-clone-b30f8.appspot.com",
-  messagingSenderId: "439105284874",
-  appId: "1:439105284874:web:762245e932be07df59a8f3",
 };
 let app;
 if (firebase.apps.length === 0) {
@@ -26,12 +20,6 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCrgaCuuTv1gQ9ZVWPeuAkei8Qiwh6jYZU",
-  authDomain: "signal-clone-b30f8.firebaseapp.com",
-  projectId: "signal-clone-b30f8",
-  storageBucket: "signal-clone-b30f8.appspot.com",
-  messagingSenderId: "439105284874",
-  appId: "1:439105284874:web:762245e932be07df59a8f3",
 };
 
 let app;
